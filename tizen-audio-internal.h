4513a873339fe3085ab3bb7e4e0ed918f00cf019
Sangchul Lee <sc11.lee@samsung.com>
2015-11-19 19:34:33 +0900

    Initialization audio HAL for SC7727 (target:TM1)
    Codes are based on audio-hal-wm5110(0.2.12)
    
    [Version] 0.1.0
    [Profile] Mobile
    [Issue Type] Initialization
    
    Change-Id: I35cd6c2132dd839d1973721a279834daf4bd352c
    Signed-off-by: Sangchul Lee <sc11.lee@samsung.com>

---
+++ tizen-audio-internal.h
@@ -0,0 +1,249 @@
+#ifndef footizenaudiointernalfoo
+#define footizenaudiointernalfoo
+
+/*
+ * audio-hal
+ *
+ * Copyright (c) 2000 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dlog.h>
+#include <time.h>
+#include <sys/types.h>
+#include <asoundlib.h>
+#ifdef __USE_TINYALSA__
+#include <tinyalsa/asoundlib.h>
+#endif
+#include <pthread.h>
+#include <use-case.h>
+#include "tizen-audio.h"
+
+/* Debug */
+
+//#define AUDIO_DEBUG
+#define PROPERTY_VALUE_MAX 92
+#define BUF_SIZE 1024
+#define AUDIO_DUMP_STR_LEN              256
+#define AUDIO_DEVICE_INFO_LIST_MAX      16
+#ifdef USE_DLOG
+#ifdef DLOG_TAG
+#undef DLOG_TAG
+#endif
+#define DLOG_TAG "AUDIO_HAL"
+#define AUDIO_LOG_ERROR(...)            SLOG(LOG_ERROR, DLOG_TAG, __VA_ARGS__)
+#define AUDIO_LOG_WARN(...)             SLOG(LOG_WARN, DLOG_TAG, __VA_ARGS__)
+#define AUDIO_LOG_INFO(...)             SLOG(LOG_INFO, DLOG_TAG, __VA_ARGS__)
+#define AUDIO_LOG_DEBUG(...)            SLOG(LOG_DEBUG, DLOG_TAG, __VA_ARGS__)
+#define AUDIO_LOG_VERBOSE(...)          SLOG(LOG_DEBUG, DLOG_TAG, __VA_ARGS__)
+#else
+#define AUDIO_LOG_ERROR(...)            fprintf(stderr, __VA_ARGS__)
+#define AUDIO_LOG_WARN(...)             fprintf(stderr, __VA_ARGS__)
+#define AUDIO_LOG_INFO(...)             fprintf(stdout, __VA_ARGS__)
+#define AUDIO_LOG_DEBUG(...)            fprintf(stdout, __VA_ARGS__)
+#define AUDIO_LOG_VERBOSE(...)          fprintf(stdout, __VA_ARGS__)
+#endif
+
+#define AUDIO_RETURN_IF_FAIL(expr) do { \
+    if (!expr) { \
+        AUDIO_LOG_ERROR("%s failed", #expr); \
+        return; \
+    } \
+} while (0)
+#define AUDIO_RETURN_VAL_IF_FAIL(expr, val) do { \
+    if (!expr) { \
+        AUDIO_LOG_ERROR("%s failed", #expr); \
+        return val; \
+    } \
+} while (0)
+
+/* Devices : Normal  */
+enum audio_device_type {
+    AUDIO_DEVICE_NONE                 = 0,
+
+    /* output devices */
+    AUDIO_DEVICE_OUT_SPEAKER          = 0x00000001,
+    AUDIO_DEVICE_OUT_RECEIVER         = 0x00000002,
+    AUDIO_DEVICE_OUT_JACK             = 0x00000004,
+    AUDIO_DEVICE_OUT_BT_SCO           = 0x00000008,
+    AUDIO_DEVICE_OUT_AUX              = 0x00000010,
+    AUDIO_DEVICE_OUT_HDMI             = 0x00000020,
+    AUDIO_DEVICE_OUT_ALL              = (AUDIO_DEVICE_OUT_SPEAKER |
+                                         AUDIO_DEVICE_OUT_RECEIVER |
+                                         AUDIO_DEVICE_OUT_JACK |
+                                         AUDIO_DEVICE_OUT_BT_SCO |
+                                         AUDIO_DEVICE_OUT_AUX |
+                                         AUDIO_DEVICE_OUT_HDMI),
+    /* input devices */
+    AUDIO_DEVICE_IN_MAIN_MIC          = 0x80000001,
+    AUDIO_DEVICE_IN_SUB_MIC           = 0x80000002,
+    AUDIO_DEVICE_IN_JACK              = 0x80000004,
+    AUDIO_DEVICE_IN_BT_SCO            = 0x80000008,
+    AUDIO_DEVICE_IN_ALL               = (AUDIO_DEVICE_IN_MAIN_MIC |
+                                         AUDIO_DEVICE_IN_SUB_MIC |
+                                         AUDIO_DEVICE_IN_JACK |
+                                         AUDIO_DEVICE_IN_BT_SCO),
+};
+
+typedef struct device_type {
+    uint32_t type;
+    const char *name;
+} device_type_t;
+
+/* Verbs */
+#define AUDIO_USE_CASE_VERB_INACTIVE                "Inactive"
+#define AUDIO_USE_CASE_VERB_HIFI                    "HiFi"
+#define AUDIO_USE_CASE_VERB_VOICECALL               "VoiceCall"
+#define AUDIO_USE_CASE_VERB_LOOPBACK                "Loopback"
+#define AUDIO_USE_CASE_VERB_FMRADIO                 "FM_Radio"
+
+/* Modifiers */
+#define AUDIO_USE_CASE_MODIFIER_VOICESEARCH              "VoiceSearch"
+#define AUDIO_USE_CASE_MODIFIER_CAMCORDING               "Camcording"
+#define AUDIO_USE_CASE_MODIFIER_RINGTONE                 "Ringtone"
+#define AUDIO_USE_CASE_MODIFIER_DUAL_RINGTONE            "DualRingtone"
+#define AUDIO_USE_CASE_MODIFIER_MEDIA                    "Media"
+#define AUDIO_USE_CASE_MODIFIER_DUAL_MEDIA               "DualMedia"
+
+#define streq !strcmp
+#define strneq strcmp
+
+#define ALSA_DEFAULT_CARD       "sc7727"
+#define VOICE_PCM_DEVICE        "hw:0,1"
+#define PLAYBACK_PCM_DEVICE     "hw:0,0"
+#define CAPTURE_PCM_DEVICE      "hw:0,0"
+
+/* hw:0,0 */
+#define PLAYBACK_CARD_ID        0
+#define PLAYBACK_PCM_DEVICE_ID  0
+
+/* hw:0,0 */
+#define CAPTURE_CARD_ID         0
+#define CAPTURE_PCM_DEVICE_ID   0
+
+#define MAX_DEVICES             5
+#define MAX_MODIFIERS           5
+#define MAX_NAME_LEN           32
+
+/* type definitions */
+typedef signed char int8_t;
+
+/* pcm */
+typedef struct {
+    snd_pcm_format_t format;
+    uint32_t rate;
+    uint8_t channels;
+} audio_pcm_sample_spec_t;
+
+/* Device */
+typedef enum audio_route_mode{
+    VERB_NORMAL,
+    VERB_CALL,
+    VERB_VOIP
+} audio_route_mode_t;
+
+typedef struct audio_device_mgr {
+    uint32_t active_in;
+    uint32_t active_out;
+    snd_pcm_t *pcm_in;
+    snd_pcm_t *pcm_out;
+    pthread_mutex_t pcm_lock;
+    uint32_t pcm_count;
+    audio_route_mode_t mode;
+} audio_device_mgr_t;
+
+
+/* Stream */
+
+#define AUDIO_VOLUME_LEVEL_MAX 16
+
+typedef struct audio_volume_value_table {
+    double volume[AUDIO_VOLUME_TYPE_MAX][AUDIO_VOLUME_LEVEL_MAX];
+    uint32_t volume_level_max[AUDIO_VOLUME_LEVEL_MAX];
+    double gain[AUDIO_GAIN_TYPE_MAX];
+} audio_volume_value_table_t;
+
+enum {
+    AUDIO_VOLUME_DEVICE_DEFAULT,
+    AUDIO_VOLUME_DEVICE_MAX,
+};
+
+typedef struct audio_volume_mgr {
+    uint32_t volume_level[AUDIO_VOLUME_TYPE_MAX];
+    audio_volume_value_table_t *volume_value_table;
+} audio_volume_mgr_t;
+
+typedef struct audio_ucm_mgr {
+    snd_use_case_mgr_t* uc_mgr;
+} audio_ucm_mgr_t;
+
+typedef struct audio_mixer_mgr {
+    snd_mixer_t *mixer;
+    pthread_mutex_t mutex;
+    struct {
+        snd_ctl_elem_value_t *value;
+        snd_ctl_elem_id_t *id;
+        snd_ctl_elem_info_t *info;
+    } control;
+} audio_mixer_mgr_t;
+
+/* Overall */
+
+typedef struct audio_mgr {
+    audio_device_mgr_t device;
+    audio_volume_mgr_t volume;
+    audio_ucm_mgr_t ucm;
+    audio_mixer_mgr_t mixer;
+} audio_mgr_t;
+
+typedef struct {
+    unsigned short      is_open; /* if is_open is true, open device; else close device.*/
+    unsigned short      is_headphone;
+    unsigned int        is_downlink_mute;
+    unsigned int        is_uplink_mute;
+} device_ctrl_t;
+
+typedef struct samplerate_ctrl {
+    unsigned int samplerate; /* change samplerate.*/
+} set_samplerate_t;
+
+audio_return_t _audio_volume_init (audio_mgr_t *am);
+audio_return_t _audio_volume_deinit (audio_mgr_t *am);
+
+audio_return_t _audio_device_init (audio_mgr_t *am);
+audio_return_t _audio_device_deinit (audio_mgr_t * am);
+audio_return_t _audio_ucm_init (audio_mgr_t *am);
+audio_return_t _audio_ucm_deinit (audio_mgr_t *am);
+void _audio_ucm_get_device_name (audio_mgr_t *am, const char *use_case, audio_direction_t direction, const char **value);
+#define _audio_ucm_update_use_case _audio_ucm_set_use_case
+audio_return_t _audio_ucm_set_use_case (audio_mgr_t *am, const char *verb, const char *devices[], const char *modifiers[]);
+audio_return_t _audio_ucm_set_devices (audio_mgr_t *am, const char *verb, const char *devices[]);
+audio_return_t _audio_ucm_set_modifiers (audio_mgr_t *am, const char *verb, const char *modifiers[]);
+int _audio_ucm_fill_device_info_list (audio_mgr_t *am, audio_device_info_t *device_info_list, const char *verb);
+int _voice_pcm_open(audio_mgr_t *am);
+int _voice_pcm_close(audio_mgr_t *am, uint32_t direction);
+audio_return_t _audio_ucm_get_verb (audio_mgr_t *am, const char **value);
+audio_return_t _audio_ucm_reset_use_case (audio_mgr_t *am);
+audio_return_t _audio_util_init (audio_mgr_t *am);
+audio_return_t _audio_util_deinit (audio_mgr_t *am);
+audio_return_t _audio_mixer_control_set_param(audio_mgr_t *am, const char* ctl_name, snd_ctl_elem_value_t* value, int size);
+audio_return_t _audio_mixer_control_set_value(audio_mgr_t *am, const char *ctl_name, int val);
+audio_return_t _audio_mixer_control_set_value_string(audio_mgr_t *am, const char* ctl_name, const char* value);
+audio_return_t _audio_mixer_control_get_value(audio_mgr_t *am, const char *ctl_name, int *val);
+audio_return_t _audio_mixer_control_get_element(audio_mgr_t *am, const char *ctl_name, snd_hctl_elem_t **elem);
+audio_return_t _audio_pcm_set_sw_params(snd_pcm_t *pcm, snd_pcm_uframes_t avail_min, uint8_t period_event);
+audio_return_t _audio_pcm_set_hw_params(snd_pcm_t *pcm, audio_pcm_sample_spec_t *sample_spec, uint8_t *use_mmap, snd_pcm_uframes_t *period_size, snd_pcm_uframes_t *buffer_size);
+uint32_t _convert_format(audio_sample_format_t format);
+#endif
---
+++ tizen-audio-volume.c
@@ -0,0 +1,398 @@
+/*
+ * audio-hal
+ *
+ * Copyright (c) 2000 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <vconf.h>
+#include <iniparser.h>
+
+#include "tizen-audio-internal.h"
+
+#define VOLUME_INI_DEFAULT_PATH     "/usr/etc/mmfw_audio_volume.ini"
+#define VOLUME_INI_TEMP_PATH        "/opt/system/mmfw_audio_volume.ini"
+#define VOLUME_VALUE_MAX            (1.0f)
+#define GAIN_VALUE_MAX              (1.0f)
+
+static const char *g_volume_vconf[AUDIO_VOLUME_TYPE_MAX] = {
+    "file/private/sound/volume/system",         /* AUDIO_VOLUME_TYPE_SYSTEM */
+    "file/private/sound/volume/notification",   /* AUDIO_VOLUME_TYPE_NOTIFICATION */
+    "file/private/sound/volume/alarm",          /* AUDIO_VOLUME_TYPE_ALARM */
+    "file/private/sound/volume/ringtone",       /* AUDIO_VOLUME_TYPE_RINGTONE */
+    "file/private/sound/volume/media",          /* AUDIO_VOLUME_TYPE_MEDIA */
+    "file/private/sound/volume/call",           /* AUDIO_VOLUME_TYPE_CALL */
+    "file/private/sound/volume/voip",           /* AUDIO_VOLUME_TYPE_VOIP */
+    "file/private/sound/volume/voice",          /* AUDIO_VOLUME_TYPE_VOICE */
+    "file/private/sound/volume/fixed",          /* AUDIO_VOLUME_TYPE_FIXED */
+};
+
+static const char *__get_volume_type_string_by_idx (uint32_t vol_type_idx)
+{
+    switch (vol_type_idx) {
+    case AUDIO_VOLUME_TYPE_SYSTEM:          return "system";
+    case AUDIO_VOLUME_TYPE_NOTIFICATION:    return "notification";
+    case AUDIO_VOLUME_TYPE_ALARM:           return "alarm";
+    case AUDIO_VOLUME_TYPE_RINGTONE:        return "ringtone";
+    case AUDIO_VOLUME_TYPE_MEDIA:           return "media";
+    case AUDIO_VOLUME_TYPE_CALL:            return "call";
+    case AUDIO_VOLUME_TYPE_VOIP:            return "voip";
+    case AUDIO_VOLUME_TYPE_VOICE:           return "voice";
+    case AUDIO_VOLUME_TYPE_FIXED:           return "fixed";
+    default:                                return "invalid";
+    }
+}
+
+static uint32_t __get_volume_idx_by_string_type (const char *vol_type)
+{
+    if (!strncmp(vol_type, "system", strlen(vol_type)) || !strncmp(vol_type, "0", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_SYSTEM;
+    else if (!strncmp(vol_type, "notification", strlen(vol_type)) || !strncmp(vol_type, "1", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_NOTIFICATION;
+    else if (!strncmp(vol_type, "alarm", strlen(vol_type)) || !strncmp(vol_type, "2", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_ALARM;
+    else if (!strncmp(vol_type, "ringtone", strlen(vol_type)) || !strncmp(vol_type, "3", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_RINGTONE;
+    else if (!strncmp(vol_type, "media", strlen(vol_type)) || !strncmp(vol_type, "4", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_MEDIA;
+    else if (!strncmp(vol_type, "call", strlen(vol_type)) || !strncmp(vol_type, "5", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_CALL;
+    else if (!strncmp(vol_type, "voip", strlen(vol_type)) || !strncmp(vol_type, "6", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_VOIP;
+    else if (!strncmp(vol_type, "voice", strlen(vol_type)) || !strncmp(vol_type, "7", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_VOICE;
+    else if (!strncmp(vol_type, "fixed", strlen(vol_type)) || !strncmp(vol_type, "8", strlen(vol_type)))
+        return AUDIO_VOLUME_TYPE_FIXED;
+    else
+        return AUDIO_VOLUME_TYPE_MEDIA;
+}
+
+static const char *__get_gain_type_string_by_idx (uint32_t gain_type_idx)
+{
+    switch (gain_type_idx) {
+    case AUDIO_GAIN_TYPE_DEFAULT:           return "default";
+    case AUDIO_GAIN_TYPE_DIALER:            return "dialer";
+    case AUDIO_GAIN_TYPE_TOUCH:             return "touch";
+    case AUDIO_GAIN_TYPE_AF:                return "af";
+    case AUDIO_GAIN_TYPE_SHUTTER1:          return "shutter1";
+    case AUDIO_GAIN_TYPE_SHUTTER2:          return "shutter2";
+    case AUDIO_GAIN_TYPE_CAMCODING:         return "camcording";
+    case AUDIO_GAIN_TYPE_MIDI:              return "midi";
+    case AUDIO_GAIN_TYPE_BOOTING:           return "booting";
+    case AUDIO_GAIN_TYPE_VIDEO:             return "video";
+    case AUDIO_GAIN_TYPE_TTS:               return "tts";
+    default:                                return "invalid";
+    }
+}
+
+static void __dump_tb (audio_mgr_t *am)
+{
+    audio_volume_value_table_t *volume_value_table = am->volume.volume_value_table;
+    uint32_t vol_type_idx, vol_level_idx, gain_type_idx;
+    const char *gain_type_str[] = {
+        "def",          /* AUDIO_GAIN_TYPE_DEFAULT */
+        "dial",         /* AUDIO_GAIN_TYPE_DIALER */
+        "touch",        /* AUDIO_GAIN_TYPE_TOUCH */
+        "af",           /* AUDIO_GAIN_TYPE_AF */
+        "shut1",        /* AUDIO_GAIN_TYPE_SHUTTER1 */
+        "shut2",        /* AUDIO_GAIN_TYPE_SHUTTER2 */
+        "cam",          /* AUDIO_GAIN_TYPE_CAMCODING */
+        "midi",         /* AUDIO_GAIN_TYPE_MIDI */
+        "boot",         /* AUDIO_GAIN_TYPE_BOOTING */
+        "video",        /* AUDIO_GAIN_TYPE_VIDEO */
+        "tts",          /* AUDIO_GAIN_TYPE_TTS */
+    };
+    char dump_str[AUDIO_DUMP_STR_LEN], *dump_str_ptr;
+
+    /* Dump volume table */
+    AUDIO_LOG_INFO("<<<<< volume table >>>>>");
+
+    const char *table_str = "volumes";
+
+    AUDIO_LOG_INFO("<< %s >>", table_str);
+
+    for (vol_type_idx = 0; vol_type_idx < AUDIO_VOLUME_TYPE_MAX; vol_type_idx++) {
+        const char *vol_type_str = __get_volume_type_string_by_idx(vol_type_idx);
+
+        dump_str_ptr = &dump_str[0];
+        memset(dump_str, 0x00, sizeof(char) * sizeof(dump_str));
+        snprintf(dump_str_ptr, 8, "%6s:", vol_type_str);
+        dump_str_ptr += strlen(dump_str_ptr);
+
+        for (vol_level_idx = 0; vol_level_idx < volume_value_table->volume_level_max[vol_type_idx]; vol_level_idx++) {
+            snprintf(dump_str_ptr, 6, "%01.2f ", volume_value_table->volume[vol_type_idx][vol_level_idx]);
+            dump_str_ptr += strlen(dump_str_ptr);
+        }
+        AUDIO_LOG_INFO("%s", dump_str);
+    }
+
+    volume_value_table = am->volume.volume_value_table;
+
+    /* Dump gain table */
+    AUDIO_LOG_INFO("<<<<< gain table >>>>>");
+
+    dump_str_ptr = &dump_str[0];
+    memset(dump_str, 0x00, sizeof(char) * sizeof(dump_str));
+
+    snprintf(dump_str_ptr, 11, "%10s", " ");
+    dump_str_ptr += strlen(dump_str_ptr);
+
+    for (gain_type_idx = 0; gain_type_idx < AUDIO_GAIN_TYPE_MAX; gain_type_idx++) {
+        snprintf(dump_str_ptr, 7, "%5s ", gain_type_str[gain_type_idx]);
+        dump_str_ptr += strlen(dump_str_ptr);
+    }
+    AUDIO_LOG_INFO("%s", dump_str);
+
+    dump_str_ptr = &dump_str[0];
+    memset(dump_str, 0x00, sizeof(char) * sizeof(dump_str));
+
+    snprintf(dump_str_ptr, 11, "%9s:", table_str);
+    dump_str_ptr += strlen(dump_str_ptr);
+
+    for (gain_type_idx = 0; gain_type_idx < AUDIO_GAIN_TYPE_MAX; gain_type_idx++) {
+        snprintf(dump_str_ptr, 7, "%01.3f ", volume_value_table->gain[gain_type_idx]);
+        dump_str_ptr += strlen(dump_str_ptr);
+    }
+    AUDIO_LOG_INFO("%s", dump_str);
+
+}
+
+static audio_return_t __load_volume_value_table_from_ini (audio_mgr_t *am)
+{
+    dictionary * dict = NULL;
+    uint32_t vol_type_idx, vol_level_idx, gain_type_idx;
+    audio_volume_value_table_t *volume_value_table = am->volume.volume_value_table;
+    int size = 0;
+
+    dict = iniparser_load(VOLUME_INI_TEMP_PATH);
+    if (!dict) {
+        AUDIO_LOG_DEBUG("Use default volume&gain ini file");
+        dict = iniparser_load(VOLUME_INI_DEFAULT_PATH);
+        if (!dict) {
+            AUDIO_LOG_WARN("Loading volume&gain table from ini file failed");
+            return AUDIO_ERR_UNDEFINED;
+        }
+    }
+
+    const char delimiter[] = ", ";
+    char *key, *list_str, *token, *ptr = NULL;
+    const char *table_str = "volumes";
+
+    /* Load volume table */
+    for (vol_type_idx = 0; vol_type_idx < AUDIO_VOLUME_TYPE_MAX; vol_type_idx++) {
+        const char *vol_type_str = __get_volume_type_string_by_idx(vol_type_idx);
+
+        volume_value_table->volume_level_max[vol_type_idx] = 0;
+        size = strlen(table_str) + strlen(vol_type_str) + 2;
+        key = malloc(size);
+        if (key) {
+            snprintf(key, size, "%s:%s", table_str, vol_type_str);
+            list_str = iniparser_getstring(dict, key, NULL);
+            if (list_str) {
+                token = strtok_r(list_str, delimiter, &ptr);
+                while (token) {
+                    /* convert dB volume to linear volume */
+                    double vol_value = 0.0f;
+                    if(strncmp(token, "0", strlen(token)))
+                        vol_value = pow(10.0, (atof(token) - 100) / 20.0);
+                    volume_value_table->volume[vol_type_idx][volume_value_table->volume_level_max[vol_type_idx]++] = vol_value;
+                    token = strtok_r(NULL, delimiter, &ptr);
+                }
+            } else {
+                volume_value_table->volume_level_max[vol_type_idx] = 1;
+                for (vol_level_idx = 0; vol_level_idx < AUDIO_VOLUME_LEVEL_MAX; vol_level_idx++) {
+                    volume_value_table->volume[vol_type_idx][vol_level_idx] = VOLUME_VALUE_MAX;
+                }
+            }
+            free(key);
+        }
+    }
+
+    /* Load gain table */
+    volume_value_table->gain[AUDIO_GAIN_TYPE_DEFAULT] = GAIN_VALUE_MAX;
+    for (gain_type_idx = AUDIO_GAIN_TYPE_DEFAULT + 1; gain_type_idx < AUDIO_GAIN_TYPE_MAX; gain_type_idx++) {
+        const char *gain_type_str = __get_gain_type_string_by_idx(gain_type_idx);
+
+        size = strlen(table_str) + strlen("gain") + strlen(gain_type_str) + 3;
+        key = malloc(size);
+        if (key) {
+            snprintf(key, size, "%s:gain_%s", table_str, gain_type_str);
+            token = iniparser_getstring(dict, key, NULL);
+            if (token) {
+                volume_value_table->gain[gain_type_idx] = atof(token);
+            } else {
+                volume_value_table->gain[gain_type_idx] = GAIN_VALUE_MAX;
+            }
+            free(key);
+        } else {
+            volume_value_table->gain[gain_type_idx] = GAIN_VALUE_MAX;
+        }
+    }
+
+    iniparser_freedict(dict);
+
+    __dump_tb(am);
+
+    return AUDIO_RET_OK;
+}
+
+audio_return_t _audio_volume_init (audio_mgr_t *am)
+{
+    int i;
+    int val = 0;
+    audio_return_t audio_ret = AUDIO_RET_OK;
+    int init_value[AUDIO_VOLUME_TYPE_MAX] = { 9, 11, 7, 11, 7, 4, 4, 7, 4, 0 };
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+
+    for (i = 0; i < AUDIO_VOLUME_TYPE_MAX; i++) {
+        am->volume.volume_level[i] = init_value[i];
+    }
+
+    for (i = 0; i < AUDIO_VOLUME_TYPE_MAX; i++) {
+        /* Get volume value string from VCONF */
+        if(vconf_get_int(g_volume_vconf[i], &val) < 0) {
+            AUDIO_LOG_ERROR("vconf_get_int(%s) failed", g_volume_vconf[i]);
+            continue;
+        }
+
+        AUDIO_LOG_INFO("read vconf. %s = %d", g_volume_vconf[i], val);
+        am->volume.volume_level[i] = val;
+    }
+
+    if (!(am->volume.volume_value_table = malloc(AUDIO_VOLUME_DEVICE_MAX * sizeof(audio_volume_value_table_t)))) {
+        AUDIO_LOG_ERROR("volume_value_table malloc failed");
+        return AUDIO_ERR_RESOURCE;
+    }
+
+    audio_ret = __load_volume_value_table_from_ini(am);
+    if(audio_ret != AUDIO_RET_OK) {
+        AUDIO_LOG_ERROR("gain table load error");
+        return AUDIO_ERR_UNDEFINED;
+    }
+
+    return audio_ret;
+}
+
+audio_return_t _audio_volume_deinit (audio_mgr_t *am)
+{
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+
+    if (am->volume.volume_value_table) {
+        free(am->volume.volume_value_table);
+        am->volume.volume_value_table = NULL;
+    }
+
+    return AUDIO_RET_OK;
+}
+
+audio_return_t audio_get_volume_level_max (void *userdata, audio_volume_info_t *info, uint32_t *level)
+{
+    audio_mgr_t *am = (audio_mgr_t *)userdata;
+    audio_volume_value_table_t *volume_value_table;
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+    AUDIO_RETURN_VAL_IF_FAIL(am->volume.volume_value_table, AUDIO_ERR_PARAMETER);
+
+    /* Get max volume level by device & type */
+    volume_value_table = am->volume.volume_value_table;
+    *level = volume_value_table->volume_level_max[__get_volume_idx_by_string_type(info->type)];
+
+    AUDIO_LOG_DEBUG("get_[%s] volume_level_max: %d", info->type, *level);
+
+    return AUDIO_RET_OK;
+}
+
+audio_return_t audio_get_volume_level (void *userdata, audio_volume_info_t *info, uint32_t *level)
+{
+    audio_mgr_t *am = (audio_mgr_t *)userdata;
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+
+    *level = am->volume.volume_level[__get_volume_idx_by_string_type(info->type)];
+
+    AUDIO_LOG_INFO("get [%s] volume_level: %d, direction(%d)", info->type, *level, info->direction);
+
+    return AUDIO_RET_OK;
+}
+
+audio_return_t audio_get_volume_value (void *userdata, audio_volume_info_t *info, uint32_t level, double *value)
+{
+    audio_mgr_t *am = (audio_mgr_t *)userdata;
+    audio_volume_value_table_t *volume_value_table;
+    char dump_str[AUDIO_DUMP_STR_LEN] = {0,};
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+    AUDIO_RETURN_VAL_IF_FAIL(am->volume.volume_value_table, AUDIO_ERR_PARAMETER);
+
+    /* Get basic volume by device & type & level */
+    volume_value_table = am->volume.volume_value_table;
+    if (volume_value_table->volume_level_max[__get_volume_idx_by_string_type(info->type)] < level)
+        *value = VOLUME_VALUE_MAX;
+    else
+        *value = volume_value_table->volume[__get_volume_idx_by_string_type(info->type)][level];
+    *value *= volume_value_table->gain[AUDIO_GAIN_TYPE_DEFAULT]; /* need to fix getting gain via audio_info_t */
+
+    AUDIO_LOG_DEBUG("get_volume_value:%d(%s)=>%f %s", level, info->type, *value, &dump_str[0]);
+
+    return AUDIO_RET_OK;
+}
+
+audio_return_t audio_set_volume_level (void *userdata, audio_volume_info_t *info, uint32_t level)
+{
+    audio_return_t audio_ret = AUDIO_RET_OK;
+    audio_mgr_t *am = (audio_mgr_t *)userdata;
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+
+    /* Update volume level */
+    am->volume.volume_level[__get_volume_idx_by_string_type(info->type)] = level;
+    AUDIO_LOG_INFO("set [%s] volume_level: %d, direction(%d)", info->type, level, info->direction);
+
+    /* set mixer related to H/W volume if needed */
+
+    return audio_ret;
+}
+
+audio_return_t audio_get_volume_mute (void *userdata, audio_volume_info_t *info, uint32_t *mute)
+{
+    audio_return_t audio_ret = AUDIO_RET_OK;
+    audio_mgr_t *am = (audio_mgr_t *)userdata;
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+
+    /* TODO. Not implemented */
+
+    return audio_ret;
+}
+
+audio_return_t audio_set_volume_mute (void *userdata, audio_volume_info_t *info, uint32_t mute)
+{
+    audio_return_t audio_ret = AUDIO_RET_OK;
+    audio_mgr_t *am = (audio_mgr_t *)userdata;
+
+    AUDIO_RETURN_VAL_IF_FAIL(am, AUDIO_ERR_PARAMETER);
+    /* TODO. Not implemented */
+
+    return audio_ret;
+}
